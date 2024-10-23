7651628f84a822c12fa05ffccb937ffeb40bab83
a23benromdor <a23benromdor@inspedralbes.cat>
2024-10-23 23:17:32 +0200

    Veure tots productes despres d'entrar a un, correccions codi i json

--- vue/js/function.js
+++ vue/js/function.js
@@ -17,10 +17,11 @@
     const visibleProd=ref(false);
     const visiblePort=ref(true);
     const visibleActual=ref(false);
+    const cestaVisible=ref(false);
     let genero=ref("all");
     const actual=reactive({nom:"",preu:"",imatge:"",genero:"", })
-    function mostrar(data){
 
+    function mostrarProducte(data){
         actual.nom=llista.zapatillas[data].nom;
         actual.preu=llista.zapatillas[data].preu;
         actual.imatge=llista.zapatillas[data].imatge;
@@ -30,9 +31,19 @@
         visibleProd.value=false;
     }
 
+    function mostrarProductes(){
+        visibleProd.value=true;
+        visibleActual.value=false;
+    }
+
+    function alternarCestella(){
+        cestaVisible.value=!cestaVisible.value;
+        console.log('Cesta alternada:', cestaVisible.value);
+    }
+
     return {
 
-            llista, visibleProd,visiblePort,visibleActual,mostrar,actual,genero 
+        llista, visibleProd, visiblePort, visibleActual, cestaVisible, mostrarProducte, mostrarProductes, alternarCestella, actual, genero 
             
             }
 
