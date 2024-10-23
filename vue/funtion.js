369cbe9c4b9a321a018fae3522cbcb8dfb0b0c0d
a23diemuj <a23diemujper@inspedralbes.cat>
2024-10-23 10:49:44 +0200

    mostrarProductes_Acabado

--- vue/funtion.js
+++ vue/funtion.js
@@ -13,20 +13,31 @@
     .then(data => {
 
 
-    llista.zapatillas=data.zapatillas;
+    llista.zapatillas=data.productes;
 
-
+        
 
 
         })
 
     const visibleProd=ref(false);
     const visiblePort=ref(true);
-    
+    const visibleActual=ref(false);
+    const actual=reactive({nom:"",preu:"",imatge:"", })
+    function mostrar(data){
+
+        actual.nom=llista.zapatillas[data].nom;
+        actual.preu=llista.zapatillas[data].preu;
+        actual.imatge=llista.zapatillas[data].imatge;
+        actual.descripcio=llista.zapatillas[data].descripcio;
+        console.log(actual)
+        visibleActual.value=true;
+        visibleProd.value=false;
+    }
 
     return {
 
-            llista, visibleProd,visiblePort 
+            llista, visibleProd,visiblePort,visibleActual,mostrar,actual 
             
             }
 
