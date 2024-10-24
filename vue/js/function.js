95afd55b84d2686508f6b640d98e14dd1f25131d
a23benromdor <a23benromdor@inspedralbes.cat>
2024-10-24 23:55:10 +0200

    Ya es guarden be els productes, falta CRUD y que s'agrupin

--- vue/js/function.js
+++ vue/js/function.js
@@ -1,4 +1,4 @@
-    import { createApp, ref, onBeforeMount, reactive} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+    import { createApp, ref, onBeforeMount, reactive, toRaw } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
     import { getProductes } from './communicationManager.js';
 
 
@@ -21,9 +21,11 @@
     const visiblePort=ref(true);
     const visibleActual=ref(false);
     const visibleCar=ref(false);
-   
+    const carList=ref([]);
+    const preuCar=ref(0);
     let genero=ref("all");
     const actual=reactive({nom:"",preu:"",imatge:"",genero:"", })
+    //const cestella = ref(null);
 
     function mostrarProducte(data){
         actual.nom=llista.zapatillas[data].nom;
@@ -31,7 +33,7 @@
         actual.imatge=llista.zapatillas[data].imatge;
         actual.descripcio=llista.zapatillas[data].descripcio;
         actual.genero=llista.zapatillas[data].genero;
-        console.log(actual.genero)
+        console.log(actual.nom);
         visibleActual.value=true;
         visibleProd.value=false;
     }
@@ -42,13 +44,32 @@
     }
 
     function alternarCestella(){
-        cestaVisible.value=!cestaVisible.value;
-        console.log('Cesta alternada:', cestaVisible.value);
+        visibleCar.value=!visibleCar.value;
+        console.log('Cesta alternada:', visibleCar.value);
+        //console.log(cestella.value);
+        //if(cestella.className===cestella.id) cestella.className="cart-obert";
+        //else cestella.className=cestella.id;
+    }
+
+    function afegirProducte(){
+        carList.value.push({...toRaw(actual)});
+        preuCarrito();
+        alternarCestella();
+    }
+
+    function preuCarrito(){
+        preuCar.value=0;
+
+        carList.value.forEach(producte => {
+            console.log(producte);
+            preuCar.value+=parseFloat(producte.preu);
+            console.log(preuCar.value);
+        });
     }
 
     return {
 
-        llista, visibleProd, visiblePort, visibleActual,visibleCar  , mostrarProducte, mostrarProductes, alternarCestella, actual, genero 
+        llista, visibleProd, visiblePort, visibleActual, visibleCar, preuCar, actual, genero, carList, mostrarProducte, mostrarProductes, alternarCestella, preuCarrito, afegirProducte 
             
             }
 
